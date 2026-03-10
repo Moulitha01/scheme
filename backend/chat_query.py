09b5aeaf8f8dd3c0f8019e67d604d8dd2f16c342
Moulitha01 <moulithac@gmail.com>
2026-03-10 22:37:02 +0530

    Update chat_query.py

--- backend/chat_query.py
+++ backend/chat_query.py
@@ -113,7 +113,7 @@ while True:
         recs = recommend_schemes(profile)
 
         if recs:
-            print("\nBot:\n")
+            print("\naganet:\n")
             for r in recs:
                 print("•", r)
 
