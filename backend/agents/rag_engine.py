23b3c0f0e71d906b819d1f86a8da9b7f0c4da87b
Moulitha01 <moulithac@gmail.com>
2026-05-03 12:33:52 +0530

    commit

--- backend/agents/rag_engine.py
+++ backend/agents/rag_engine.py
@@ -6,7 +6,7 @@ from langchain_ollama import OllamaLLM
 
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+
 
 
 DB_PATH = "faiss_index"
